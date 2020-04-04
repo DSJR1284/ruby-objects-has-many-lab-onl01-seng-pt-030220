7f8ad2422a3fd4f4d1655c52668b1f0ce8e0bc65
Doug Jones <dsjr1284@gmail.com>
2020-04-04 09:43:00 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,19 +1,19 @@
-class Artist
-  attr_accessor :name, :songs
-  @@song_count
-
-  def initialize
-    @name=name
-
-  end
-
-  def add_song
-
-  end
-
-  def song_by_name(song_name)
-
-  end
-
-
-end
+# class Artist
+#   attr_accessor :name, :songs
+#   @@song_count
+#
+#   def initialize
+#     @name=name
+#
+#   end
+#
+#   def add_song
+#
+#   end
+#
+#   def song_by_name(song_name)
+#
+#   end
+#
+#
+# end
