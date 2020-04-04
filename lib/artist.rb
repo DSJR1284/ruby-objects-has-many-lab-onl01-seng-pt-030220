e925126205caf336f8de503ccc2d890d71eb62fe
Doug Jones <dsjr1284@gmail.com>
2020-04-04 09:45:13 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,17 +3,16 @@ class Artist
   @@song_count
 
   def initialize
-    @name=name
-
   end
-
-  def add_song
-
-  end
-
-  def song_by_name(song_name)
-
-  end
-
-
-end
+  
+# 
+#   def add_song
+# 
+#   end
+# 
+#   def song_by_name(song_name)
+# 
+#   end
+# 
+# 
+# end
